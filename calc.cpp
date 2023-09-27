7ee0a348bf23487d999af4ce63943c86123cd9fc
inner <inner049@gmail.com>
2023-09-27 09:54:10 +0300

    calc

--- calc.cpp
+++ calc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ int main() {
 
   cout << "Введіть перше число\n";   cin >> a;
 
-  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n5\t**\n\n";  cin >> op;
 
   cout << "Введіть друге число\n";     cin >> b;
 
@@ -18,15 +19,19 @@ int main() {
 
                     break;
 
-    case 2:    // code
+    case 2:     result = a - b;
 
                     break;
 
-    case 3:     // code
+    case 3:     result = a * b;
 
                     break;
 
-    case 4:     // code
+    case 4:     result = (double) a / b;
+
+                    break;
+
+    case 5:     result = pow(a, b);
 
                     break;
 
